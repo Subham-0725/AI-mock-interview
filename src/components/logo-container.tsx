318a13a0dc65ecbcc289848f0a69cc7d806c4b3f
Subham Satyajit <subham.satyajit05@gmail.com>
2025-08-15 02:27:17 +0530

    Home Page designed

--- src/components/logo-container.tsx
+++ src/components/logo-container.tsx
@@ -1,18 +1,17 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 const LogoContainer = () => {
   return (
     <div>
       <Link to={"/"}>
-      <img
-        src="/assets/svgs/logo.svg"
-        alt="Logo"
-        className="min-w-10 min-h-10 object-contain"
-      />
-      
+        <img
+          src="/assets/svg/Mockzi.jpg"
+          alt="Logo"
+          className="w-14 h-17 object-cover rounded-full"
+        />
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default LogoContainer
+export default LogoContainer;
